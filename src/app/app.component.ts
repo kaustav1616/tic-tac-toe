81af522d27743e7cf00a9509ad9cd8f98b4454ab
kaustav1616 <kaustav1616@yahoo.com>
2021-08-12 12:08:01 +0530

    full angular code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tic-tac-toe';
+export class AppComponent
+{
+	title = 'tic-tac-toe';
 }
